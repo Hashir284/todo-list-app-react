7b7d6a70b2f4a531273e7c03ec66c25b9c687e2d
muhammadibsam63 <onlyforprofile1@gmail.com>
2026-06-06 06:00:00 -0700

    PUSH

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,72 @@
-import './App.css';
-import { useState } from 'react';
-import Tr from './component/tr';
+import "./App.css";
+import { useState } from "react";
+import Tr from "./component/tr";
 
 function App() {
-  const [task, stask] = useState('')
-  const [arr, sarr] = useState([])
+  const [task, stask] = useState("");
+  const [arr, sarr] = useState([]);
+
+  const del = (index) => {
+    let filteredArray = arr.filter((e, i) => {
+      return i !== index;
+    });
+    sarr(filteredArray);
+  };
 
   return (
     <div className="App">
-      <h1>To-Do List</h1>
-      <form onSubmit={(e)=>{
-        e.preventDefault()
-        sarr([...arr, task])
-        console.log(arr);
+      <h1>  
+        To-Do List
+      </h1>
         
-      }}>
-    <div>
-      <input type="text" placeholder='Add your task' onChange={(e)=>{
-        console.log(e);
-        stask(e.target.value)
-      }}/>
-      <button>Add</button>
-    </div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (task !== "") {
+            const newtask = {
+              id: Date.now(),
+              text: task 
+            }
+            sarr([...arr, newtask]);
+            stask('');
+            console.log(arr);
+          } else {
+            alert('Please! Enter the task');
+          }
+        }}
+      >
+        <div className="asker">
+          <input
+            type="text"
+            placeholder="Add your task"
+            value={task}
+            className="input"
+            onChange={(e) => {
+              console.log(e);
+              if (e.target.value !== "") {
+                stask(e.target.value);
+              }
+              if(e.target.value === ""){
+                stask('')
+              }
+            }}
+          />
+          <button className="add-btn">ADD</button>
+        </div>
       </form>
 
       <table>
-        {
-          arr.map((e)=>{
-            return <Tr task={e}/>
-          })
-        }        
-      </table>
+        {arr.map((e, i) => {
+          return <Tr task={e.text} key={e.id} del={del} index={e.id} i={i} />;
+        })}
+        <div className="pending">
+          <h3>You have {arr.length} pending Tasks</h3>
+          <button onClick={()=>{
+            sarr([])
+          }}>Clear all</button>
+        </div>
 
+      </table>
     </div>
   );
 }
--- src/component/tr.jsx
+++ src/component/tr.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./../App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
-const Tr = ({task}) => {
+const Tr = ({ task, index, del, i }) => {
+
+  const [checked, schecked] = useState("");
+
   return (
     <tr>
-        <td className='td1'>{task}</td>
-        <td className='td2'><button>Delete</button></td>
+      <td className="td1">
+        <input
+          type="checkbox"
+          name=""
+          className="checkbox"
+          id={'e'+index}
+          onChange={(e) => {
+            console.log(e.target.checked);
+            schecked(e.target.checked);
+          }}
+        />
+        <label htmlFor={'e'+index}>{checked ? <s className="c">{task}</s> : task}</label>
+      </td>
+      <td className="td2">
+        <div
+          onClick={() => {
+            del(i);
+          }}
+        >
+          <FontAwesomeIcon icon={faXmark} />
+        </div>
+      </td>
     </tr>
-  )
-}
+  );
+};
 
-export default Tr
+export default Tr;
